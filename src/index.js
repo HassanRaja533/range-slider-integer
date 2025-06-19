d7badd4306fe3e383d0a4403de8d763ed733f166
Hassan <h.hassanraja1000@gmail.com>
2025-06-20 02:58:53 +0500

    fix formatting

--- src/index.js
+++ src/index.js
@@ -4,58 +4,47 @@ const integer = require('input-integer-ui-hassan_raja')
 module.exports = range_slider_integer
 
 function range_slider_integer (opts) {
-    const state ={}
+  const state = {}
   const el = document.createElement('div')
   const shadow = el.attachShadow({ mode: 'closed' })
+
   const range_slider = range(opts, protocol)
   const input_integer = integer(opts, protocol)
-  const style = document.createElement('style')
 
+  const style = document.createElement('style')
   style.textContent = get_theme()
+
   const output = document.createElement('div')
   output.classList.add('output')
   output.innerText = 0
+
   shadow.append(range_slider, input_integer, style, output)
 
   return el
 
-  function protocol(message, notify){
-    const {from} = message
-    state[from] = {value:0,notify}
+  function protocol (message, notify) {
+    const { from } = message
+    state[from] = { value: 0, notify }
     return listen
   }
 
   // handler
   function listen (message) {
-    const { from,type, data } = message
+    const { from, type, data } = message
     state[from].value = data
-    console.log(state)
     if (type === 'update') {
       output.innerText = data
     }
   }
 
-  
   function get_theme () {
     return `
       .output {
-      border :1px solid red;
+        border :1px solid red;
         padding: 20px;
         text-align: center;
         width: 200px;
       }
     `
   }
-
-//   function get_theme () {
-//     return `
-//       .rsi {
-//         padding: 5%;
-//         display: grid;
-//         grid-template-columns: 8fr 1fr;
-//         align-items: center;
-//         justify-items: center;
-//       }
-//     `
-//   }
 }
